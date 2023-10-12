b34fc77ad1b1cd9ad3bba5e19e476ac31c380f53
Egor Dubovik <egorka54896@gmail.com>
2023-10-12 18:00:13 +0300

    init: add base project structure and config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	poweredByHeader: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		MODE: process.env.REACT_APP_MODE,
+		SERVER_URL: process.env.REACT_APP_SERVER_URL,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,130 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
+import colors from 'tailwindcss/colors';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+
+	theme: {
+		colors: {
+			primary: '#e30b13',
+			black: colors.black,
+			while: colors.white,
+			transparent: colors.transparent,
+			yellow: {
+				700: '#f5c521',
+			},
+			gray: {
+				300: '#d9dae8',
+				400: '#999aa5',
+				500: '#66676e',
+				600: '#39393f',
+				700: '#242529',
+				800: '#191b1f',
+				900: '#101215',
+			},
+		},
+		extend: {
+			spacing: {
+				0.5: '0.12rem',
+				layout: '2.75rem',
+			},
+			fontSize: {
+				'2lg': '1.38rem',
+			},
+			borderRadius: {
+				image: '0.5rem',
+				layout: '0.8rem',
+			},
+			transitionTimingFunction: {
+				default: 'ease-in-out',
+			},
+			transitionDuration: {
+				default: '200ms',
+			},
+			zIndex: {
+				1: '1',
+				2: '2',
+				3: '3',
+			},
+			keyframes: {
+				fade: {
+					from: { opacity: '0' },
+					to: { opacity: '1' },
+				},
+				scaleIn: {
+					'0%': {
+						opacity: '0',
+						transform: 'scale(0.9)',
+					},
+					'50%': {
+						opacity: '0.3',
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'scale(1)',
+					},
+				},
+			},
+			animation: {
+				fade: 'fade 0.5s ease-in-out',
+				scaleIn: 'scaleIn 0.35s ease-in-out',
+			},
+		},
+	},
+	plugins: [
+		plugin(({ addComponents, addUtilities, theme }) => {
+			addComponents({
+				'.button-primary': {
+					backgroundColor: theme('colors.primary'),
+					borderRadius: '0.65rem',
+					color: '#fff',
+					transition: 'background-color 0.3 ease-in-out',
+					'&:hover': {
+						backgroundColor: '#ff0009',
+					},
+				},
+				'text-link': {
+					color: 'rgba(255, 255, 255 0.9)',
+					transition: 'text-decoration-color 0.3 ease-in-out',
+					textDecorationLine: 'underline',
+					textDecorationColor: 'rgba(255, 255, 255 0.2)',
+					textUnderlineOffset: '4',
+					'&:hover': {
+						textDecorationColor: 'rgba(255, 255, 255 0.9)',
+					},
+				},
+				'.air-block': {
+					backgroundColor: theme('colors.gray.900'),
+					borderRadius: theme('borderRadius.layout'),
+					color: theme('colors.white'),
+					boxShadow: theme('boxShadow.lg'),
+				},
+			});
+			addUtilities({
+				'.text-shadow': {
+					textShadow: '1px 1px rgba(0, 0, 0, 0.4)',
+				},
+				'.outline-border-none': {
+					outline: 'none',
+					border: 'none',
+				},
+				'.flex-center-between': {
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'space-between',
+				},
+				'.image-link-bg': {
+					objectFit: 'cover',
+					objectPosition: 'center',
+					pointerEvents: 'none',
+				},
+			});
+		}),
+	],
+};
+export default config;
